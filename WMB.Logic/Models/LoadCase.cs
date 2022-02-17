c22d078ac55be824f33eeddb66d113db5be47236
miravaculka <miravaculka@gmail.com>
2022-02-17 13:38:33 +0100

    mapping to LoadCases

---
+++ WMB.Logic/Models/LoadCase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMB.Logic.Models
+{
+    internal class LoadCase
+    {
+        public string? Name { get; set; }
+        public int LoadStatesQuantity { get; set; }
+        public double TimeShare { get; set; }
+    }
+}
--- WMB.Logic/Processor.cs
+++ WMB.Logic/Processor.cs
@@ -1,4 +1,5 @@
 ﻿using Helper;
+using WMB.Logic.Models;
 
 namespace WMB.Logic
 {
@@ -13,8 +14,20 @@ namespace WMB.Logic
             {
                 throw new Exception("Load case list není vyplněný.");
             }
-            Dictionary<(int, int), string> result = FileProcessor.LoadDataFromFile_csvSemicolon(LoadCaseListPath);
-
+            // Dictionary<int, string> ... key : int ... unikátní; value: string 
+            Dictionary<(int, int), string> loadCaseData = FileProcessor.LoadDataFromFile_csvSemicolon(LoadCaseListPath);
+            // Mapping Dictionary to List of Load Case:
+            List<LoadCase> loadCases = new List<LoadCase>();
+            // => .. lambda expression
+            for (int row = 0; row <= loadCaseData.Select(lcd => lcd.Key.Item1).Max(); row++)
+            {
+                LoadCase loadCase = new LoadCase
+                {
+                    Name = loadCaseData[(row, 0)],
+                    TimeShare = Convert.ToDouble(loadCaseData[(row, 1)])
+                };
+                loadCases.Add(loadCase);
+            }
         }
 
     }
